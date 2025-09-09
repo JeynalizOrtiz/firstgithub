0cf4cf06475ad6631c474b50f3a5660784655804
Jeynaliz.Ortiz <jeynaog19@gmail.com>
2025-09-09 09:52:44 -0400

    R code for figure

---
+++ My_first_plot.R
@@ -0,0 +1,39 @@
+#Build a simple graph
+
+#libraries
+library(ggplot2)
+library(EVR628tools)
+
+#load data
+
+data("data_heatwaves")
+
+#Build a plot
+
+heat <- ggplot(data= data_heatwaves,
+               mapping = aes( x= year, y= temp_mean,
+                              group = paste(fishery, eu_rnpa))) +
+  geom_point(color = "steelblue")
+
+heat
+
+#Export my plot
+
+ggsave(plot = heat,
+       filename= "results/img/my_first_plot.png")
+
+
+#Build another plot
+
+data("data_lionfish")
+
+lionfishplot <- ggplot(data = data_lionfish,
+                       mapping = aes(x = site, y = total_length_mm))+
+  geom_point(color= "red")
+
+lionfishplot
+
+ggsave(plot = lionfishplot,
+       filename = "results/img/my_second_plot.png")
+
+x
